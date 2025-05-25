a969d6c29c59d13f9589b83ce397ee8698df4775
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-05-25 17:00:35 +0700

    improve/formInput

--- src/core/components/forms/formInput.tsx
+++ src/core/components/forms/formInput.tsx
@@ -4,12 +4,8 @@ type FormInputProps = {
     value: string | number
     type?: string
     placeholder?: string
-    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void
-    required?: boolean
-    icon?: React.ReactNode
     disabled?: boolean
-    className?: string
-    textarea?: boolean
+    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => void // Tipe yang lebih fleksibel
 }
 
 const inputClass = "w-full border border-gray-300 focus:border-blue-500 focus:outline-none p-2 rounded-md text-sm appearance-none bg-white";
@@ -22,44 +18,18 @@ export const FormInput = ({
     onChange,
     type = "text",
     placeholder = "",
-    required = false,
-    icon,
-    disabled = false,
-    className = "",
-    textarea = false
+    disabled = false
 }: FormInputProps) => (
     <div>
-        <label className={labelClass}>
-            {label}
-        </label>
-        <div className="relative flex items-center">
-            {icon && (
-                <div className="absolute left-3">
-                    {icon}
-                </div>
-            )}
-            {textarea ? (
-                <textarea
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    placeholder={placeholder}
-                    required={required}
-                    disabled={disabled}
-                    className={`${inputClass} ${icon ? "pl-10" : ""} ${className}`}
-                />
-            ) : (
-                <input
-                    name={name}
-                    value={value}
-                    onChange={onChange}
-                    type={type}
-                    placeholder={placeholder}
-                    required={required}
-                    disabled={disabled}
-                    className={`${inputClass} ${icon ? "pl-10" : ""} ${className}`}
-                />
-            )}
-        </div>
+        <label className={labelClass}>{label}</label>
+        <input
+            name={name}
+            value={value}
+            onChange={onChange}
+            type={type}
+            placeholder={placeholder}
+            className={inputClass}
+            disabled={disabled}
+        />
     </div>
 );
